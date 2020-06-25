3f78e41a07247afaa20a59727dedad5b3dfa9be8
GavinMelis576 <59562883+GavinMelis576@users.noreply.github.com>
2020-06-25 13:35:54 +0200

    het vul programma

---
+++ IoT_casus_Application2/Classes/Device.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoT_casus_Application2
+{
+    class Device
+    {
+        public int idx { get; set; }
+        public string Name { get; set; }
+        public int PlanID { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+
+    }
+}
